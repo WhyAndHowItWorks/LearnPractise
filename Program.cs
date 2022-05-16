a6b5116eb1291c257181dae62091748349406e23
Абросимов Андрей 5137 <andrejabrosimow@yandex.ru>
2022-05-17 01:08:43 +0300

    Добавлено чтение файла

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.IO;
 
-namespace LearnPractise
+namespace LearnPractiseXD
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите путь к файлу пожалуйста!");
+            string path = Console.ReadLine();
+            string originalText = File.ReadAllText(path);
+            Console.WriteLine(originalText);
         }
     }
 }
